82dde3822f1d9a17f25b64bf97d9c7211bd562e5
oomis <damilolaomiyera@email.com>
2019-07-23 02:54:33 +0000

    add name to devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_20_221229) do
+ActiveRecord::Schema.define(version: 2019_07_23_023732) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -41,6 +41,9 @@ ActiveRecord::Schema.define(version: 2019_07_20_221229) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.datetime "date_of_birth"
+    t.boolean "is_female", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
