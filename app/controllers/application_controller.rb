fcd9d83de1f0e10a49d4e6c2f0c35168e6efb913
oomis <damilolaomiyera@email.com>
2019-07-27 08:54:07 +0000

    changed view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,14 @@
 class ApplicationController < ActionController::Base
     
+    before_action :getCatSection
+    
     protect_from_forgery with: :exception
 
     before_action :configure_permitted_parameters, if: :devise_controller?
+    
+    def getCatSection
+        @catSections = Category.all.order("created_at DESC")
+    end
 
     protected
 
