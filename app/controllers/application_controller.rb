a76784018df865db5a4f078443e2567a66037bbf
oomis <damilolaomiyera@email.com>
2019-08-02 21:39:42 +0000

    more frontend

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
     
-    before_action :getCatSection
+    before_action :getCatSection, :getPostSection
     
     protect_from_forgery with: :exception
 
@@ -8,6 +8,10 @@ class ApplicationController < ActionController::Base
     
     def getCatSection
         @catSections = Category.all.order("created_at DESC")
+    end
+    
+    def getPostSection
+        @postSections = Post.all.order("created_at DESC")
     end
 
     protected
